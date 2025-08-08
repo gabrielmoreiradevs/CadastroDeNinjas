29c2227133dacedb0a68e983742bf2a3fb8af28c
Gabriel Moreira <gabrielmoreirafermino@hotmail.com>
2025-08-08 14:26:00 -0300

    feat(model): criar entidade NinjaModel com atributos nome, email e idade
    Implementada a classe NinjaModel como uma entidade JPA para representar
    um ninja no banco de dados. Inclui os atributos nome, email e idade,
    além de construtores, getters e setters.
    
    Closes: #2

---
+++ src/main/java/dev/gabrielmoreira/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,56 @@
+package dev.gabrielmoreira.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+//JPA = Java persistence API
+//Entity transforma uma classe em uma entidade do Banco de dados.
+@Entity
+//Table Cria uma tabela com as colunas(Definindo o nome da tabela inteira!)
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    //Definindo o ID com a annotation
+    @Id
+    //Passando a estrategia de definir a sequencia dos ID's
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private int idade;
+
+    //NoArgs
+    public NinjaModel() {
+    }
+    //AllArgs
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    //Getter and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
