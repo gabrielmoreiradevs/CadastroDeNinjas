44e1f491f2bc9e8c36b4cb910bee6f511133f6d2
Gabriel Moreira <gabrielmoreirafermino@hotmail.com>
2025-08-11 16:38:01 -0300

    feat(model): adicionar Lombok nas entidades NinjaModel e MissaoModel
    Incluídas as anotações @Data, @NoArgsConstructor e @AllArgsConstructor do Lombok
    nas classes NinjaModel e MissaoModel para reduzir código boilerplate de getters,
    setters, construtores e métodos utilitários.

--- src/main/java/dev/gabrielmoreira/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/gabrielmoreira/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.gabrielmoreira.CadastroDeNinjas.Missoes;
 
 import dev.gabrielmoreira.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name="tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/gabrielmoreira/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/gabrielmoreira/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,14 +2,23 @@ package dev.gabrielmoreira.CadastroDeNinjas.Ninjas;
 
 import dev.gabrielmoreira.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 //JPA = Java persistence API
 //Entity transforma uma classe em uma entidade do Banco de dados.
 @Entity
+
 //Table Cria uma tabela com as colunas(Definindo o nome da tabela inteira!)
 @Table(name = "tb_cadastro")
+
+//Trabalhando com Lombok, tirando o gasto de ficar fazendo overload.
+@NoArgsConstructor
+@AllArgsConstructor
+@Data //Cria todos Getter's and Setter's
 public class NinjaModel {
 
     //Definindo o ID com a annotation
@@ -27,38 +36,4 @@ public class NinjaModel {
     @JoinColumn(name="missoes_id")//Foreina Key ou Chave Estrangeira
     private MissoesModel missoes;
 
-    //NoArgs
-    public NinjaModel() {
-    }
-    //AllArgs
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    //Getter and Setters
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
