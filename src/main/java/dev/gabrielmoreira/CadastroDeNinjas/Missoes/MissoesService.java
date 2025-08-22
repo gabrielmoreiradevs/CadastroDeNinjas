70df27536ec0bb66ee4cfb60c57af33c685d6565
Gabriel Moreira <gabrielmoreirafermino@hotmail.com>
2025-08-22 17:17:02 -0300

    feaat: inclui DTO como abstração em toda minha aplicação

--- src/main/java/dev/gabrielmoreira/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/gabrielmoreira/CadastroDeNinjas/Missoes/MissoesService.java
@@ -3,6 +3,7 @@ package dev.gabrielmoreira.CadastroDeNinjas.Missoes;
 import dev.gabrielmoreira.CadastroDeNinjas.Ninjas.NinjaModel;
 import dev.gabrielmoreira.CadastroDeNinjas.Ninjas.NinjaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,6 +36,19 @@ public class MissoesService {
         MissoesModel missoes = missoesMapper.map(missoesDTO);
         missoes =  missoesRepository.save(missoes);
         return missoesMapper.map(missoes);
+    }
+
+    public MissoesDTO alterarMissoesPorId(Long idMissoes, MissoesDTO missoesDTO){
+        Optional<MissoesModel> missoesModel = missoesRepository.findById(idMissoes);
+
+        if(missoesModel.isPresent()){
+            MissoesModel missoes = missoesMapper.map(missoesDTO);
+            missoes.setId(idMissoes);
+
+            MissoesModel missoesSalva = missoesRepository.save(missoes);
+            return missoesMapper.map(missoesSalva);
+        }
+        return null;
     }
 
     public void deletarMissoesPorId(Long idMissoes){ missoesRepository.deleteById(idMissoes); }
